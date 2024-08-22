748d68f002d35738ce2f2ff44e2b4e996e2fce61
Samuel Andres Alvarez Yepes <Samus2978@gmail.com>
2024-08-22 11:06:56 -0500

    Cree la clase Todo e inicialice los parametros

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -7,37 +7,4 @@ class Todo:
         self.tags: list = []
         self.completed: bool = False
 
-    def mark_completed(self):
-        self.completed = True
-
-    def add_tag(self, tag: str):
-        if tag not in self.tags:
-            self.tags.append(tag)
-
-    def __str__(self):
-        return f"{self.code_id} - {self.title}"
-
-
-class TodoBook:
-    def __init__(self, todos: dict):
-        self.todos: dict = {}
-
-    def add_todo(self, title: str, description: str) -> int:
-        new_id = len(self.todos) + 1
-        new_todo = Todo(new_id, title, description)
-        self.todos[new_id] = new_todo
-        return new_id
-
-    def pending_todos(self) -> Todo:
-        new_list = [todo for todo in self.todos.values() if not Todo.completed]
-        return new_list
-
-    def completed_todos(self) -> Todo:
-        new_list = [todo for todo in self.todos.values() if Todo.completed]
-        return new_list
-
-    def tags_todo_count(self) -> dict:
-        tag_count = {}
-        for todo in self.todos
-
 
